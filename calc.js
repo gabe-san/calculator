77aa899752931894d98aa14e8257e6e5d65dc3c9
gabe-san <gsantosx98@gmail.com>
2023-03-12 16:40:17 -0700

    described initial logic and function of basic arithmetic operations

--- calc.js
+++ calc.js
@@ -1,5 +1,22 @@
 
 //add function
+const addition = function(a,b) {
+    return a + b
+}
+
 //subtract function
+const subtraction = function(a,b) {
+    return a - b
+}
 //multiple function
+const multiplication = function (a,b) {
+    return a * b
+}
 //divide function
+
+//input two numbers
+//function takes two numbers and divides the two numbers
+//return the number and remainder if applicable
+const division = function(a,b) {
+    
+}
